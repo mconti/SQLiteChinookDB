a7015b041d0ad2b0070eef2687c72d34e6981dcd
Maurizio Conti <mconti@users.noreply.github.com>
2023-05-11 10:47:54 +0200

    .

---
+++ conti.maurizio.4H.GUIDb/conti.maurizio.4H.GUIDb/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using SQLite;
+
+namespace conti.maurizio._4H.GUIDb
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var cn1 = new SQLiteConnection("chinook.db");
+
+            List<Artist> tblArtists;
+
+            // Prende tutti gli artisti
+            tblArtists = cn1.Query<Artist>("select * from artists where name like 'a%'");
+            dgDati.ItemsSource = tblArtists;
+
+        }
+    }
+}
